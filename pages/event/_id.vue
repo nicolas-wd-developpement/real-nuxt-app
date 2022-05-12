3ecc23c4d7cba07a25552e25ab25810df3332112
nicolasblond@web-designers.fr <nicolasblond@web-designers.fr>
2022-05-12 09:21:08 +0200

    Adding axios and event api call

---
+++ pages/event/_id.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1> Event #{{ id }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        head() { // <-- property used by vue-meta to add header tags
+            return {
+            title: 'Event # ' + this.id, // <-- For our title tag
+            meta: [
+                    {
+                    hid: 'description event #' + this.id,  
+                    name: 'description event #' + this.id, // <-- for our meta description tag
+                    content:
+                        'here you can see the description of the event #' + this.id
+                    }
+                ]
+            }
+        },
+        computed: {
+            id() { return this.$route.params.id } 
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/event/createEvent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Create anEvent</h1>
+  </div>
+</template>
+
+<script >
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    head() { // <-- property used by vue-meta to add header tags
+        return {
+        title: 'Event create', // <-- For our title tag
+        meta: [
+                {
+                hid: 'description',  
+                name: 'description', // <-- for our meta description tag
+                content:
+                    'Where you can create all the events taking place in your neighborhood'
+                }
+            ]
+        }
+    }
+  })
+</script>
+
+<style>
+
+</style>
